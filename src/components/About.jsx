c5f735be44c829e701127c54d04eab17df7f0f48
anandpopalwar <anandpopalwar444@gmail.com>
2023-06-26 13:09:11 +0530

    2nd commit fevicon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,22 +9,22 @@ const About = () => {
 
         <p >
           Hey <span className='text-black underline ' >Anand here</span> , recent <span className='text-black underline ' >B.Sc Graduate specialization in Computer Science</span>  </p>
-          <p > i am a Passionate   <span className='text-black underline ' >Front-End React Developer</span> dedicated to creating seamless user experiences.
-          </p>
+        <p > i am a Passionate   <span className='text-black underline ' >Front-End React Developer</span> dedicated to creating seamless user experiences.
+        </p>
 
-        <p>   
-           Skilled in<span className='text-black underline' > HTML, CSS, and JavaScript,</span> I specialize in leveraging   <span className='text-black underline' >React JS</span> to bring ideas to life.
-           </p>
+        <p>
+          Skilled in<span className='text-black underline' > HTML, CSS, and JavaScript,</span> I specialize in leveraging   <span className='text-black underline' >React JS</span> to bring ideas to life.
+        </p>
         <p>
           With an eye for detail and commitment to clean code, I deliver   <span className='text-black underline' >visually appealing</span> and optimized web applications.
         </p>
-   
+
         <p>
           with <span className='text-black underline' >
-         Responsive web design   </span>
-             using CSS libreries such as<span className='text-black underline' > Tailwind and Bootstrap   </span>
-           </p>
-      
+            Responsive web design   </span>
+          using CSS libreries such as<span className='text-black underline' > Tailwind and Bootstrap   </span>
+        </p>
+
         <a href="mailto:anandpopalwar444@gmail.com" className=' text-black hover:underline  hover:transition ease-in-out    hover:-translate-y-1 hover:scale-110  duration-300' target='blank'> @ anandpopalwar444@gmail.com </a>
 
       </div>
